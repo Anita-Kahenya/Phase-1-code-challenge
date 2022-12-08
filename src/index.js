6bef1e723971ec506aa7b3f8d23b4f9f62c3cd44
Renee Mumbi <1723renee@gmail.com>
2022-12-08 15:44:06 +0300

    Challenge completed

---
+++ src/index.js
@@ -0,0 +1,53 @@
+let characterDiv=document.getElementById("character-bar")
+
+let detailedInfoDiv=document.getElementById("detailed-info")
+
+let formDetails=document.getElementById("votes-form")
+let name=document.getElementById("name")
+let image=document.getElementById("image")
+let form=document.getElementById("votes-form")
+let button=document.getElementById("button")
+let voteElement=document.getElementById("vote-count")
+
+
+const renderName=(animalsData)=>{
+    //let spanElement
+    animalsData.forEach(element => {
+        
+    //    console.log(element.name)
+   let spanElement=document.createElement("span")
+     spanElement.textContent=element.name
+    characterDiv.appendChild(spanElement)
+
+    spanElement.addEventListener("click",renderDetails )
+    function renderDetails(event){
+        name.textContent=element.name
+        image.src=element.image
+        voteElement.textContent=addedVotes
+        image.alt=element.name
+     
+    }
+    
+    
+   
+    });
+
+    function renderVotes(event){
+        event.preventDefault()
+        let addedVotes=document.getElementById("votes").value
+        console.log(addedVotes)
+        voteElement.textContent=addedVotes
+    }
+    form.addEventListener("submit", renderVotes)
+    //console.log(spanElement)
+ 
+    
+}
+
+
+fetch("http://localhost:3000/characters")
+.then((response)=>response.json())
+.then((data)=>renderName(data))
+
+
+
