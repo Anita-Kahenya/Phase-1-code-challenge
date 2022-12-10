4292474a5af8cafdc2d222deb7aec29bd5003e21
Renee Mumbi <1723renee@gmail.com>
2022-12-10 23:15:44 +0300

    Comment added

--- src/index.js
+++ src/index.js
@@ -1,56 +1,9 @@
-// let characterDiv=document.getElementById("character-bar")
 
-// let detailedInfoDiv=document.getElementById("detailed-info")
-
-// let formDetails=document.getElementById("votes-form")
-// let name=document.getElementById("name")
-// let image=document.getElementById("image")
-// let form=document.getElementById("votes-form")
-// let button=document.getElementById("button")
-// let voteElement=document.getElementById("vote-count")
-
-
-// const renderName=(animalsData)=>{
-//     //let spanElement
-//     animalsData.forEach(element => {
-        
-//     //    console.log(element.name)
-//    let spanElement=document.createElement("span")
-//      spanElement.textContent=element.name
-//     characterDiv.appendChild(spanElement)
-
-//     spanElement.addEventListener("click",renderDetails )
-//     function renderDetails(event){
-//         name.textContent=element.name
-//         image.src=element.image
-//         voteElement.textContent=addedVotes
-//         image.alt=element.name
-     
-//     }
-    
-    
-   
-//     });
-
-//     function renderVotes(event){
-//         event.preventDefault()
-//         let addedVotes=document.getElementById("votes").value
-//         console.log(addedVotes)
-//         voteElement.textContent=addedVotes
-//     }
-//     form.addEventListener("submit", renderVotes)
-//     //console.log(spanElement)
- 
-    
-// }
-
-
-// fetch("http://localhost:3000/characters")
-// .then((response)=>response.json())
-// .then((data)=>renderName(data))
 document.addEventListener("DOMContentLoaded",() => {
     fetchingData();
 });
+
+//Creating the variables that are going to be in use 
 const baseUrl = "http://localhost:3000/characters"
 const characterBar = document.getElementById("character-bar");
 const characterName = document.getElementById("name");
@@ -58,7 +11,7 @@ const characterImage = document.getElementById("image");
 const characterVoteCount = document.getElementById("vote-count")
 const voteForm = document.getElementById("votes-form")
 
-
+// Hoisting the function that will be used to fetch the data 
 function fetchingData(){
     fetch(baseUrl)
     .then((response)=>response.json())
@@ -67,7 +20,7 @@ function fetchingData(){
         createCharacters(data)
     })
 }
-
+// creating the function that will create the span element 
 function createCharacters(data){
     data.map((data)=>{
         const createSpan=document.createElement("span")
@@ -84,11 +37,19 @@ function createCharacters(data){
     })
 }
 
+// adding an event listener for the votes button 
 voteForm.addEventListener("submit", (event) =>{
     event.preventDefault();
+    
     const freshVotes=parseInt(event.target.votes.value)
     
     let currentVotes=parseInt(characterVoteCount.textContent)
     let finalVotes= (currentVotes+=freshVotes)
     characterVoteCount.textContent=finalVotes
+
+    
 })
+
+
+//Bonus Deliverables
+const resetBtn = document.getElementById("reset-btn")
