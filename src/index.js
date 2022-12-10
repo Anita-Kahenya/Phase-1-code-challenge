436ce2a351298d5c7da011ecd2f6aa62d26ee7ee
Renee Mumbi <1723renee@gmail.com>
2022-12-10 17:21:40 +0300

    create variables to fetch api

--- src/index.js
+++ src/index.js
@@ -1,53 +1,53 @@
-let characterDiv=document.getElementById("character-bar")
+// let characterDiv=document.getElementById("character-bar")
 
-let detailedInfoDiv=document.getElementById("detailed-info")
+// let detailedInfoDiv=document.getElementById("detailed-info")
 
-let formDetails=document.getElementById("votes-form")
-let name=document.getElementById("name")
-let image=document.getElementById("image")
-let form=document.getElementById("votes-form")
-let button=document.getElementById("button")
-let voteElement=document.getElementById("vote-count")
+// let formDetails=document.getElementById("votes-form")
+// let name=document.getElementById("name")
+// let image=document.getElementById("image")
+// let form=document.getElementById("votes-form")
+// let button=document.getElementById("button")
+// let voteElement=document.getElementById("vote-count")
 
 
-const renderName=(animalsData)=>{
-    //let spanElement
-    animalsData.forEach(element => {
+// const renderName=(animalsData)=>{
+//     //let spanElement
+//     animalsData.forEach(element => {
         
-    //    console.log(element.name)
-   let spanElement=document.createElement("span")
-     spanElement.textContent=element.name
-    characterDiv.appendChild(spanElement)
+//     //    console.log(element.name)
+//    let spanElement=document.createElement("span")
+//      spanElement.textContent=element.name
+//     characterDiv.appendChild(spanElement)
 
-    spanElement.addEventListener("click",renderDetails )
-    function renderDetails(event){
-        name.textContent=element.name
-        image.src=element.image
-        voteElement.textContent=addedVotes
-        image.alt=element.name
+//     spanElement.addEventListener("click",renderDetails )
+//     function renderDetails(event){
+//         name.textContent=element.name
+//         image.src=element.image
+//         voteElement.textContent=addedVotes
+//         image.alt=element.name
      
-    }
+//     }
     
     
    
-    });
+//     });
 
-    function renderVotes(event){
-        event.preventDefault()
-        let addedVotes=document.getElementById("votes").value
-        console.log(addedVotes)
-        voteElement.textContent=addedVotes
-    }
-    form.addEventListener("submit", renderVotes)
-    //console.log(spanElement)
+//     function renderVotes(event){
+//         event.preventDefault()
+//         let addedVotes=document.getElementById("votes").value
+//         console.log(addedVotes)
+//         voteElement.textContent=addedVotes
+//     }
+//     form.addEventListener("submit", renderVotes)
+//     //console.log(spanElement)
  
     
-}
+// }
 
 
-fetch("http://localhost:3000/characters")
-.then((response)=>response.json())
-.then((data)=>renderName(data))
+// fetch("http://localhost:3000/characters")
+// .then((response)=>response.json())
+// .then((data)=>renderName(data))
 
-
-
+const baseUrl="http://localhost:3000/characters"
+const characterBar=document.querySelector("#character-bar")
